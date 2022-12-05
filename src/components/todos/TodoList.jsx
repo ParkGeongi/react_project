2334946aef189146ef643e5faac82e04874610d1
ParkGeongi <pgg113579@naver.com>
2022-12-05 17:25:25 +0900

    2022/12/05

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -7,6 +7,7 @@ const Todolist = () => {
     const dispatch = useDispatch()
     const toggleTodo = id => dispatch(toggleTodoAction(id))
     const deleteTodo = id => dispatch(deleteTodoAction(id))
+    
     return (<>
 
         <h2>할일 목록</h2>
@@ -21,9 +22,16 @@ const Todolist = () => {
         { todos.length === 0 && (<tr>
             <td>등록된 할 일이 없습니다</td>
         </tr>)}
-        {todos.length !== 0 && (<tr>
-            <td>등록된 할 일이 있습니다</td>
-        </tr>)}
+        
+        
+        
+        {todos.length !== 0 && todos.map(todo => (<tr key={todo.id}>
+            <td> <input type="checkbox" checked = {todo.complete} 
+            onChange={toggleTodo.bind(null, todo.id)}/> {todo.complete ? <span style={{textDecoration: "line-through"}}>{todo.name}</span> : 
+                                                                        <span>{todo.name}</span>}
+            <button onClick={deleteTodo.bind(null,todo.id)}>x</button>
+        </td>
+        </tr>))}
 
         </tbody>
         </table>
